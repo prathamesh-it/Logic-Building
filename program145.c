50388ab1fb05789bfac67d98baeb276447dd1927
prathamesh-it <prathameshnistane1412@gmail.com>
2025-01-26 18:34:40 +0530

    Add files via upload

---
+++ program145.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main()
+{
+    char Name[]="Mumbai";
+
+    printf("%s\n",Name);         //100 -->Base address of array
+
+
+    printf("%c\n",Name[0]);
+    printf("%c\n",Name[1]);
+    printf("%c\n",Name[2]);
+    printf("%c\n",Name[3]);
+    
+
+    return 0;
+
+}

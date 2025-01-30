50306ad71961654f650b9682917ad6c2ec24e467
prathamesh-it <prathameshnistane1412@gmail.com>
2025-01-30 18:34:29 +0530

    Add files via upload

---
+++ program181.c
@@ -0,0 +1,36 @@
+//Change the small letter of string to capital letter and capital to small
+
+#include<stdio.h>
+
+int StrtoggleX(char *str)     
+{
+    while(*str != 0)
+    {
+        if(*str >= 'a' && *str<='z')
+        {
+            *str=*str-32;
+        }
+        else if(*str >= 'A' && *str<='Z')
+        {
+            *str=*str+32;
+        }       
+        str++;
+    }
+
+}
+
+int main()
+{
+    char Arr[30];
+    
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    StrtoggleX(Arr);
+
+    printf("String after updation is : %s\n",Arr);
+
+
+    return 0;
+}
+

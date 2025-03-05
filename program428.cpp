3d5030b5eb46c1127ee040e888f87d198fd2243b
prathamesh-it <prathameshnistane1412@gmail.com>
2025-03-05 19:20:21 +0530

    Add files via upload

---
+++ program428.cpp
@@ -0,0 +1,296 @@
+// Generic Doubly Linear Linked List
+
+#include<iostream>
+using namespace std;
+
+template <class T>
+
+struct node
+{
+    T data;
+    struct node *next;
+    struct node *prev;
+};
+
+template <class T>
+class DoublyLL
+{
+    public:
+        struct node <T> * head;
+        int iCount;
+
+        DoublyLL();
+        void InsertFirst(T no);
+        void InsertLast(T no);
+        void DeleteFirst();
+        void DeleteLast();
+        void Display();
+        int Count();
+        void InsertAtPos(T no , int ipos);
+        void DeleteAtPos(int ipos);           
+
+};
+
+template <class T>
+DoublyLL <T> :: DoublyLL()
+{
+    head = NULL;
+    iCount = 0;
+}
+
+template <class T>
+void DoublyLL <T> :: InsertFirst(T no)
+{
+    struct node <T> * newn = NULL;
+
+    newn = new node <T>;
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;
+
+    if(head==NULL)
+    {
+        head=newn;
+    }
+    else
+    {
+        newn->next=head;
+        head->prev=newn;
+        head=newn;
+    }
+    iCount++;
+
+}
+
+template <class T>
+void DoublyLL <T> :: InsertLast(T no)
+{
+    struct node <T> * newn = NULL;
+    struct node <T> * temp = head;
+
+    newn = new node <T>;
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;
+
+    if(head==NULL)
+    {
+        head=newn;
+    }
+    else
+    {
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=newn;
+        newn->prev=temp;
+    }
+    iCount++;
+
+}
+
+template <class T>
+void DoublyLL <T> :: DeleteFirst()
+{
+    struct node <T> * temp = head;
+
+    if(head==NULL)
+    {
+        return;
+    }
+    else if(head->next==NULL)
+    {
+        delete head;
+        head=NULL;
+    }
+    else
+    {
+        head=head->next;
+        head->prev=NULL;
+        delete temp;
+    }
+    iCount--;
+
+}
+
+template <class T>
+void DoublyLL <T> :: DeleteLast()
+{
+    struct node <T> * temp = head;
+
+    if(head==NULL)
+    {
+        return;
+    }
+    else if(head->next==NULL)
+    {
+        delete head;
+        head=NULL;
+    }
+    else
+    {
+        temp=head;
+        
+        while(temp->next->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        delete temp->next;
+        temp->next=NULL;
+    }
+    iCount--;
+}
+
+template <class T>
+void DoublyLL <T> :: Display()
+{
+    struct node <T> * temp=head;
+
+    cout<<"Elements of the linked are:\n";
+    
+    cout<<"NULL<=>";
+    while(temp!=NULL)
+    {
+        cout<<"| " <<temp->data <<" |<=>";
+        temp=temp->next;
+    }
+    printf("NULL\n");
+}
+
+template <class T>
+int DoublyLL <T> :: Count()
+{
+    return iCount;
+}
+
+template <class T>
+void DoublyLL  <T>:: InsertAtPos(T no , int ipos)
+{
+    int i = 0;
+
+    struct node <T> * newn = NULL;
+    struct node <T> * temp = NULL;
+
+    if((ipos<1) || (ipos>iCount+1))
+    {
+        cout<<"Invalid position";
+        return;
+    }
+
+    if(ipos==1)
+    {
+        InsertFirst(no);
+    }
+    if(ipos==iCount+1)
+    {
+        InsertLast(no);
+    }
+    else
+    {
+        newn = new node <T>;
+
+        newn->data=no;
+        newn->next = NULL;
+        newn->prev = NULL;
+
+        temp = head;
+
+        for(i=1 ; i<ipos-1 ; i++)
+        {
+            temp=temp->next;
+        }
+        newn->next=temp->next;
+        temp->next->prev=newn;
+        temp->next = newn;
+        newn->prev = temp;
+
+        iCount++;
+
+    }
+    
+}
+
+template <class T>
+void DoublyLL <T> :: DeleteAtPos(int ipos)
+{
+    int Countnode = 0;
+    int i = 0;
+
+    struct node <T> * target = NULL;
+    struct node <T> * temp = NULL;
+
+    Countnode = Count();
+
+    if((ipos<1) || (ipos>Countnode+1))
+    {
+        cout<<"Invalid position";
+        return;
+    }
+
+    if(ipos==1)
+    {
+        DeleteFirst();
+    }
+    if(ipos==Countnode)
+    {
+        DeleteLast();
+    }
+    else
+    {
+        temp = head;
+
+        for(i=1 ; i<ipos-1 ; i++)
+        {
+            temp = temp->next;
+        }
+        target = temp->next;
+
+        temp->next=target->next;
+        temp->next->prev = temp;
+
+        delete target;
+
+        iCount--;
+
+    }
+    
+}
+
+
+int main()
+{
+    DoublyLL <char> dobj;
+    int iRet = 0;
+
+    dobj.InsertFirst('A');
+    dobj.InsertFirst('B');
+    dobj.InsertFirst('C');
+
+    dobj.InsertLast('D');
+    dobj.InsertLast('E');
+    dobj.InsertLast('F');
+
+    dobj.Display();
+
+    iRet = dobj.Count();
+    cout<<"Number of elements are :"<<iRet<<"\n";
+
+    dobj.InsertAtPos('J' , 4);
+
+    dobj.Display();
+
+    iRet = dobj.Count();
+    cout<<"Number of elements are :"<<iRet<<"\n";
+
+    dobj.DeleteAtPos(4);
+
+    dobj.Display();
+
+    iRet = dobj.Count();
+    cout<<"Number of elements are :"<<iRet<<"\n";
+
+    return 0;
+}
---
+++ program430.cpp
@@ -0,0 +1,282 @@
+//Gneric Singly Circular LinkedList
+
+#include<iostream>
+using namespace std;
+
+template <class T>
+struct node
+{
+    T data;
+    struct node *next;
+};
+
+template <class T>
+class SinglyCL
+{
+    public:
+        struct node <T> * head;
+        struct node <T> * tail;
+        int iCount;
+
+        SinglyCL();
+        void InsertFirst(T no);
+        void InsertLast(T no);
+        void DeleteFirst();
+        void DeleteLast();
+        void Display();
+        int Count();
+        void InsertAtPos(T no , int ipos);
+        void DeleteAtPos(int ipos);
+
+        
+};
+
+template <class T>
+SinglyCL <T>:: SinglyCL()
+{
+    head = NULL;
+    tail = NULL;
+    iCount = 0;
+}
+
+template <class T>
+void SinglyCL <T> :: InsertFirst(T no)
+{
+    struct node <T> * newn = NULL;
+
+    newn = new node <T>;
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if((head==NULL)&&(tail==NULL))
+    {
+        head = newn;
+        tail = newn;
+    }
+    else
+    {
+        newn->next = head;
+        head = newn;
+    }
+    tail->next = head;
+
+    iCount++;
+}
+
+template <class T>
+void SinglyCL <T> :: InsertLast(T no)
+{
+    struct node <T> * newn = NULL;
+
+    newn = new node <T>;
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if((head==NULL)&&(tail==NULL))
+    {
+        head = newn;
+        tail = newn;
+    }
+    else
+    {
+        tail->next = newn;
+        tail = newn;
+    }
+    tail->next = head;
+
+    iCount++;
+}
+
+template <class T>
+void SinglyCL <T> :: DeleteFirst()
+{
+    if((head==NULL)&&(tail==NULL))
+    {
+        return;
+    }
+    else if(head==tail)
+    {
+        delete head;
+        head = NULL;
+        tail = NULL;
+    }
+    else
+    {
+        head = head->next;
+        delete tail->next;
+
+        tail->next = head;
+    }
+
+    iCount--;
+}
+
+template <class T>
+void SinglyCL <T> :: DeleteLast()
+{
+    struct node <T> * temp = NULL;
+
+    if((head==NULL)&&(tail==NULL))
+    {
+        return;
+    }
+    else if(head==tail)
+    {
+        delete head;
+        head = NULL;
+        tail = NULL;
+    }
+    else
+    {
+        temp = head;
+
+        while(temp->next!=tail)
+        {
+            temp = temp->next;
+        }
+        delete temp->next;
+        tail = temp;
+
+        tail->next = head;
+    }
+
+    iCount--;
+}
+
+template <class T>
+void SinglyCL <T> :: Display()
+{
+    struct node <T> * temp = NULL;
+
+    temp = head;
+
+    do
+    {
+        cout<<"| "<<temp->data<<"|->";
+        temp = temp->next;
+    } while (temp!=tail->next);
+    printf("\n");
+    
+
+}
+
+template <class T>
+int SinglyCL <T> :: Count()
+{
+    return iCount;
+}
+
+template <class T>
+void SinglyCL <T> :: InsertAtPos (T no , int ipos)
+{
+    struct node <T> * newn = NULL;
+    struct node <T> * temp = NULL;
+
+    int i =0;
+
+    if((ipos<1) || (ipos>iCount+1))
+    {
+        cout<<"Invalid position\n";
+        return;
+    }
+
+    if(ipos==1)
+    {
+        InsertFirst(no);
+    }
+    else if(ipos==iCount+1)
+    {
+        InsertLast(no);
+    }
+    else
+    {
+        newn = new node <T>;
+
+        newn->data = no;
+        newn->next = NULL;
+
+        temp = head;
+
+        for(i = 1 ; i<ipos-1;i++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+
+        iCount++;
+    }
+}
+
+template <class T>
+void SinglyCL <T> :: DeleteAtPos(int ipos)
+{
+    struct node <T> * temp = NULL;
+    struct node <T> * target = NULL;
+
+    int i =0;
+
+    if((ipos<1) || (ipos>iCount))
+    {
+        cout<<"Invalid position\n";
+        return;
+    }
+
+    if(ipos==1)
+    {
+        DeleteFirst();
+    }
+    else if(ipos==iCount+1)
+    {
+        DeleteLast();
+    }
+    else
+    {
+        
+        temp = head;
+
+        for(i = 1 ; i<ipos-1;i++)
+        {
+            temp = temp->next;
+        }
+        target = temp->next;
+
+        temp->next = target->next;
+        delete target;
+
+        iCount--;
+    } 
+}
+
+
+int main()
+{
+    SinglyCL <float> sobj;
+    int iRet = 0;
+
+    sobj.InsertFirst(51.7);
+    sobj.InsertFirst(21.2);
+    sobj.InsertFirst(11.3);
+
+    sobj.InsertLast(101.5);
+    sobj.InsertLast(111.8);
+    sobj.InsertLast(121.7);
+
+    sobj.Display();
+    iRet = sobj.Count();
+    cout<<"Number of elements in LinkedList are :"<<iRet<<"\n";
+
+    sobj.InsertAtPos(75.5 , 4);
+    sobj.Display();
+    iRet = sobj.Count();
+    cout<<"Number of elements in LinkedList are :"<<iRet<<"\n";
+
+    sobj.DeleteAtPos(4);
+    sobj.Display();
+    iRet = sobj.Count();
+    cout<<"Number of elements in LinkedList are :"<<iRet<<"\n";
+
+    return 0;
+}

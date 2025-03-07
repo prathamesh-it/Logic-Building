ab9a8fd1a29396446e7d576c5916657511a39e5d
prathamesh-it <prathameshnistane1412@gmail.com>
2025-03-07 08:28:00 +0530

    Add files via upload

---
+++ program523.java
@@ -0,0 +1,28 @@
+
+class node
+{
+    public int data;
+    public node next;
+}
+
+class SinglyLL
+{
+    public node head;
+    public int iCount;
+
+    public SinglyLL()
+    {
+        head = null;   //this.head = null
+        iCount = 0;
+    }
+}
+
+class program523
+{
+    public static void main (String args[])
+    {
+        SinglyLL sobj = new SinglyLL();
+
+        
+    }
+}
